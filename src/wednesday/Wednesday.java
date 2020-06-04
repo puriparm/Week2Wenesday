3b3bdb0736e743cb044676d789ab77a99b5f5a9b
puriparm <puriparm@sherdiancollege.ca>
2020-06-04 08:31:26 -0400

    first commit

---
+++ src/wednesday/Wednesday.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package wednesday;
+
+/**
+ *
+ * This class +++Insert Description Here+++
+ * 
+ * @author Parminder Kaur
+ */
+public class Wednesday {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        
+        System.out.println("learning new about github");
+    }
+
+}
